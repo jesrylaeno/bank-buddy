95de4e4f22f01f7300294500d9a282adc74cdf05
jesrylaeno <73213497+jesrylaeno@users.noreply.github.com>
2025-07-04 18:21:51 +0800

    004: add initial sign-up page

--- bank-buddy-fe/src/app/app.routes.ts
+++ bank-buddy-fe/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
+import { SignupComponent } from './pages/sign-up/sign-up';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   // More routes like /signup or /forgot-password
 ];
